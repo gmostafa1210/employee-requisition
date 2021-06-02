0d65944f5eecce8f2d86c7ab0e2c8b76e3ee7312
Golam Mostafa <sujon1mostafa@gmail.com>
2021-06-02 17:47:47 +0600

    employee requisition basic structure with security and buttons

--- models/requisition.py
+++ models/requisition.py
@@ -7,7 +7,34 @@ class EmployeeRequisition(models.Model):
 
     name = fields.Many2one('hr.employee', string='Employee Name')
     date = fields.Date(string="Date")
+    requisition_state = fields.Char(string='Requisition State', default='Draft')
+
     requisition_line = fields.One2many('employee.requisition.line', 'requisition_id', string='Requisition Line')
+
+
+    state = fields.Selection([('draft', 'Draft'), 
+                ('requisition', 'Requisition'),
+                ('approved', 'Approved'),
+                ('confirmed', 'Confirmed'),
+                ('cancelled', 'Cancelled')], 
+                default='draft', string='State')
+
+    def action_requisition(self):
+        self.state = 'requisition'
+        self.requisition_state = 'Draft'
+
+    def action_approved(self):
+        self.state = 'approved'
+        self.requisition_state = 'Approved'
+
+    def action_confirmed(self):
+        self.state = 'confirmed'
+        self.requisition_state = 'Confirmed'
+
+    def action_cancelled(self):
+        self.state = 'cancelled'
+        self.requisition_state = 'Cancelled'
+
 
 class EmployeeRequisitionLine(models.Model):
     _name = 'employee.requisition.line'
