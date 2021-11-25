f6c08dcc6dcac5632f3b1f751bf795d4fe5c78cf
gmostafa1210 <gmostafa1210@gmail.com>
2021-11-26 01:04:00 +0600

    [ADD] chatter

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,17 @@
 {
-    'name' : 'Employee Requisition',
-    'version' : '1.0',
-    'sequence' : 1,
+    'name': 'Employee Requisition',
+    'version': '1.0',
+    'sequence': 1,
     'summary': 'Employee Requisition Summary',
-    'description' : 'Employee Requisition Description.',
-    'depends' : ['base', 'hr', 'purchase'],
-    'data' : [
+    'description': 'Employee Requisition Description.',
+    'depends': ['base', 'hr', 'purchase', 'mail'],
+    'data': [
         'security/requisition_security.xml',
         'security/ir.model.access.csv',
 
         'views/requisition_views.xml',
     ],
-    'installable' : True,
-    'application' : True,
-    'auto_install' : False,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
--- models/requisition.py
+++ models/requisition.py
@@ -1,22 +1,27 @@
 from odoo import api, models, fields, _
 from odoo.exceptions import UserError
 
+
 class EmployeeRequisition(models.Model):
     _name = 'employee.requisition'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Employee Requisition'
 
-    name = fields.Many2one('hr.employee', string='Employee Name')
+    name = fields.Many2one(
+        'hr.employee', string='Employee Name')
     date = fields.Date(string="Date")
-    requisition_state = fields.Char(string='Requisition State', default='Draft')
+    requisition_state = fields.Char(
+        string='Requisition State', default='Draft')
 
-    requisition_line = fields.One2many('employee.requisition.line', 'requisition_id', string='Requisition Line')
+    requisition_line = fields.One2many(
+        'employee.requisition.line', 'requisition_id', string='Requisition Line')
 
-    state = fields.Selection([('draft', 'Draft'), 
-                ('confirmed', 'Confirmed'),
-                ('approved', 'Approved'),
-                ('ready', 'Ready'),
-                ('received', 'Received')], 
-                default='draft', string='State')
+    state = fields.Selection([('draft', 'Draft'),
+                              ('confirmed', 'Confirmed'),
+                              ('approved', 'Approved'),
+                              ('ready', 'Ready'),
+                              ('received', 'Received')],
+                             default='draft', string='State')
 
     def action_confirmed(self):
         self.state = 'confirmed'
@@ -34,11 +39,32 @@ class EmployeeRequisition(models.Model):
         self.state = 'received'
         self.requisition_state = 'Received'
 
+    def write(self, values):
+        msg = "<b>" + \
+            _("The employee requisition has been updated.") + "</b><ul>"
+        if 'name' in values.keys():
+            new = self.env['hr.employee'].browse(values['name']).name
+            old = self.name.name
+            msg += "<li>" + \
+                _("Employee Name: %s to %s") % (old, new) + "</li>"
+
+        if 'date' in values.keys():
+            new = values['date']
+            old = self.date
+            msg += "<li>" + \
+                _("Date: %s to %s") % (old, new) + "</li>"
+
+        self.message_post(body=msg)
+
+        return super(EmployeeRequisition, self).write(values)
+
 
 class EmployeeRequisitionLine(models.Model):
     _name = 'employee.requisition.line'
     _description = 'Employee Requisition'
 
-    product_id = fields.Many2one('product.template', string='Product Name')
+    product_id = fields.Many2one(
+        'product.template', string='Product Name')
     product_qty = fields.Integer(string='Quantity', default=1)
-    requisition_id = fields.Many2one('employee.requisition', string='Requisition By')
+    requisition_id = fields.Many2one(
+        'employee.requisition', string='Requisition By')
